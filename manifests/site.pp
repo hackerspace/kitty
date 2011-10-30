a38f6e0fc8644b86b9c7beef9554ea28306ba49c
Richard Marko <rissko@gmail.com>
2011-10-30 20:20:17 +0100

    Add common packages

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,8 @@
 node default {
   include motd
+
+  $common_packages = ["vim-enhanced", "wget", "strace", "ltrace", "screen", "git" ]
+  package { $common_packages:
+    ensure => installed,
+  }
 }

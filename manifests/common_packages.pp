1d7261a693f5ce1184eb221fdbd053aeb461ef4c
Richard Marko <rissko@gmail.com>
2011-10-30 22:03:50 +0100

    Add mail clients

--- manifests/common_packages.pp
+++ manifests/common_packages.pp
@@ -1,5 +1,5 @@
 class common_packages {
-  $common_packages = ["vim-enhanced", "wget", "strace", "ltrace", "screen", "git" ]
+  $common_packages = ["vim-enhanced", "wget", "strace", "ltrace", "screen", "git", "mutt", "mailx" ]
   package { $common_packages:
     ensure => installed,
   }

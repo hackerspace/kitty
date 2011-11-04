b7f62e510f9fd5c4257e8988452a913b6e442bea
Richard Marko <rissko@gmail.com>
2011-11-04 17:44:26 +0100

    Improve web manifest

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,7 +1,31 @@
+user { "web":
+  ensure => "present",
+  gid => "web",
+}
+
+file {
+  "/var/web": ensure => directory, owner => web,
+  require => User["web"];
+  "/var/web/src": ensure => directory, owner => web,
+  require => User["web"];
+  "/var/web/env": ensure => directory, owner => web,
+  require => User["web"];
+}
+
 include webapp::python
+
+
+
+class { "webapp::python": owner => "web",
+                          group => "web",
+                          src_root => "/var/web/src",
+                          venv_root => "/var/web/env",
+                          monit_admin => "root@localhost",
+}
 
 webapp::python::instance { "test":
   domain => "test.base48.cz",
   django => true,
   requirements => true,
+  requires => User["web"],
 }

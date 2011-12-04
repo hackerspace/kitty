6e426bcd12c09db139673795b6f22dd001f58210
Richard Marko <rissko@gmail.com>
2011-12-04 20:56:43 +0100

    Fix repository baseurl

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -1,6 +1,6 @@
 class repos {
   yumrepo { "supervisor":
-    baseurl  => 'http://repos.fedorapeople.org/repos/rmarko/supervisor/epel-$releasever/$basearch/',
+    baseurl  => 'http://repos.fedorapeople.org/repos/rmarko/supervisor/epel-6/noarch/',
     descr    => 'supervisord for epel',
     enabled  => 1,
     gpgcheck => 0

0947f1042966299f71e4a5e2b988d247a3928b62
Richard Marko <rissko@gmail.com>
2011-12-04 20:53:32 +0100

    Fix wrong parameter

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -1,8 +1,8 @@
 class repos {
   yumrepo { "supervisor":
-    baseurl => 'http://repos.fedorapeople.org/repos/rmarko/supervisor/epel-$releasever/$basearch/',
-    description => 'supervisord for epel',
-    enabled => 1,
+    baseurl  => 'http://repos.fedorapeople.org/repos/rmarko/supervisor/epel-$releasever/$basearch/',
+    descr    => 'supervisord for epel',
+    enabled  => 1,
     gpgcheck => 0
   }
 }
